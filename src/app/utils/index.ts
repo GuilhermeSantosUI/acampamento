2a73fb83ae5a9b3887baad95b179b4f5d728c084
“guiherme” <guilherme@agapesistemas.com.br>
2025-01-22 23:03:16 -0300

    adiciona exportações de funções utilitárias para formatação de texto, moeda, data, tamanhos de arquivo e máscaras

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+export * from './capitalize-first-letter';
+export * from './currency-string-number';
+export * from './format-currency';
+export * from './format-date';
+export * from './format-file-size';
+export * from './masks';
+export * from './normalize-text';
+
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
