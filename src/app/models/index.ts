49b19776c0514da639917a50e743a20469b8cd8a
“guiherme” <guilherme@agapesistemas.com.br>
2025-01-22 22:51:39 -0300

    adiciona tipos AuthProps e RegisterProps para autenticação e registro de usuários; cria arquivo de índice para exportação de modelos

---
+++ src/app/models/index.ts
@@ -0,0 +1,4 @@
+export * from './branch';
+export * from './event';
+export * from './participant';
+export * from './user';
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,3 +2,17 @@ export type UserProps = {
   id: number;
   username: string;
 };
+
+export type AuthProps = {
+  token: string;
+  userId: number;
+  expires: Date;
+};
+
+export type RegisterProps = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  branch: number;
+};
