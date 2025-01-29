f3586fb6fbf8549c6add33dfbd37cda5f47ccc7d
“guiherme” <guilherme@agapesistemas.com.br>
2025-01-29 18:22:20 -0300

    feat(auth): utiliza chaves de configuração para armazenamento de token de acesso

--- src/app/context/auth-context.tsx
+++ src/app/context/auth-context.tsx
@@ -1,53 +1,55 @@
 import { createContext, useCallback, useState } from 'react';
+
+import { localStorageKeys } from '../config';
 import { authService } from '../services/auth';
 
 type AuthState = {
-    token: string;
+  token: string;
 };
 
 type AuthContextValue = {
-    signedIn: boolean;
-    signIn(login: string, password: string): Promise<void>;
-    signOut(): void;
+  signedIn: boolean;
+  signIn(login: string, password: string): Promise<void>;
+  signOut(): void;
 };
 
 export const AuthContext = createContext({} as AuthContextValue);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [authState, setAuthState] = useState<AuthState | null>(() => {
-        const token = localStorage.getItem('ACCESS_TOKEN');
-        if (token) {
-            return { token };
-        }
-        return null;
-    });
+  const [authState, setAuthState] = useState<AuthState | null>(() => {
+    const token = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+    if (token) {
+      return { token };
+    }
+    return null;
+  });
 
-    const signIn = useCallback(async (login: string, password: string) => {
-        try {
-            const { token } = await authService.auth({ login, password });
+  const signIn = useCallback(async (login: string, password: string) => {
+    try {
+      const { token } = await authService.auth({ login, password });
 
-            localStorage.setItem('ACCESS_TOKEN', token);
+      localStorage.setItem(localStorageKeys.ACCESS_TOKEN, token);
 
-            setAuthState({ token });
-        } catch (error) {
-            console.error('Login failed:', error);
-            throw new Error('Login failed');
-        }
-    }, []);
+      setAuthState({ token });
+    } catch (error) {
+      console.error('Erro ao fazer login: ' + error);
+      throw new Error('Login failed');
+    }
+  }, []);
 
-    const signOut = useCallback(() => {
-        localStorage.removeItem('ACCESS_TOKEN');
-        setAuthState(null);
-    }, []);
+  const signOut = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+    setAuthState(null);
+  }, []);
 
-    return (
-        <AuthContext.Provider
-            value={{
-                signedIn: !!authState,
-                signIn,
-                signOut,
-            }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        signedIn: !!authState,
+        signIn,
+        signOut,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
