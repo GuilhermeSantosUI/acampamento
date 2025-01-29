411fe2f92cb0dec97c0593105d156fd13516793f
“guiherme” <guilherme@agapesistemas.com.br>
2025-01-29 18:27:54 -0300

    feat(auth): adiciona hook useAuth e ajusta importações para caminhos absolutos

--- src/app/context/auth-context.tsx
+++ src/app/context/auth-context.tsx
@@ -1,7 +1,6 @@
-import { createContext, useCallback, useState } from 'react';
-
-import { localStorageKeys } from '../config';
-import { authService } from '../services/auth';
+import { localStorageKeys } from '@/app/config';
+import { authService } from '@/app/services/auth';
+import { createContext, useCallback, useContext, useState } from 'react';
 
 type AuthState = {
   token: string;
@@ -52,4 +51,14 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       {children}
     </AuthContext.Provider>
   );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
 }
