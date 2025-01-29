2d29d1e76c7e046f5604ab432f46f3e9fe29ebed
“guiherme” <guilherme@agapesistemas.com.br>
2025-01-29 18:52:21 -0300

    feat(toaster): adiciona componente Toaster e integra ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { MainContext } from '@/app/context';
 import { Router } from '@/router';
+import { Toaster } from '@/views/components/ui/sonner';
 
 export function App() {
   return (
     <MainContext>
       <Router />
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            padding: '16px',
+            gap: '16px',
+          },
+          className: 'class',
+        }}
+        richColors
+      />
     </MainContext>
   );
 }
