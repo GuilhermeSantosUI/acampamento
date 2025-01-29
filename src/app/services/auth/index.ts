a477acb2a9708deab898a6eda9a9b14f292960cd
“guiherme” <guilherme@agapesistemas.com.br>
2025-01-29 18:28:21 -0300

    fix(auth): remove import duplicado do módulo auth.ts

--- src/app/services/auth/index.ts
+++ src/app/services/auth/index.ts
@@ -1,7 +1,7 @@
+import { auth } from './auth.ts';
 import { getAll } from './get-all';
 import { register } from './register';
 import { verifyUsername } from './verify-username';
-import {auth} from "./auth.ts";
 
 export const authService = {
   auth,
